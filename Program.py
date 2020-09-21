5a952ca23e864dd4d7b6e103686dc0fd243e43ff
Tom-Bartie <71287129+Tom-Bartie@users.noreply.github.com>
2020-09-21 15:17:29 -0400

    Update Program.py

--- Program.py
+++ Program.py
@@ -1,2 +1,3 @@
 name = input("Please enter your name")
-word = input("Please enter a word")
+word = (input("Please enter a word")).upper()
+print("Users name is: ", name," Word in capital letters: ", word)
